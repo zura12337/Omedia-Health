3131cbdb683ec1ea8261ace6052f84100c08e11a
zura12337 <zura.kurta@gmail.com>
2020-07-25 13:58:29 +0400

    Updates

---
+++ precache-manifest.93cdf271c668c9440fd203925a16569c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160bc3d4a4e6962c9f8f4f95aaefeaaf",
+    "url": "/Omedia-Health/index.html"
+  },
+  {
+    "revision": "de19af94d8877f4c4025",
+    "url": "/Omedia-Health/static/css/main.7ea955c5.chunk.css"
+  },
+  {
+    "revision": "2e79256b8fec5c5aff83",
+    "url": "/Omedia-Health/static/js/2.7fb3fe5e.chunk.js"
+  },
+  {
+    "revision": "4c796ab9273dad0140c4d6224d61af40",
+    "url": "/Omedia-Health/static/js/2.7fb3fe5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de19af94d8877f4c4025",
+    "url": "/Omedia-Health/static/js/main.0650e382.chunk.js"
+  },
+  {
+    "revision": "49093578baa5fe2fd762",
+    "url": "/Omedia-Health/static/js/runtime-main.5c21cccb.js"
+  }
+]);
